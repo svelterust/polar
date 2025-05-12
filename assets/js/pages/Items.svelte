77dec2b0499448302ee1bc728dcd97f9abec6776
SvelteRust <oddharald@myhren.ai>
2025-05-12 13:09:21 +0200

    svelte just too good mate

--- assets/js/pages/Items.svelte
+++ assets/js/pages/Items.svelte
@@ -43,19 +43,14 @@
     }
 </script>
 
-<h1 class="text-3xl font-bold mb-2">Create Item</h1>
+<h1 class="mb-2 text-3xl font-bold">Create Item</h1>
 <div class="mb-8 max-w-screen-sm">
     <form onsubmit={createItem}>
         <div class="form-control mb-4">
             <span class="label-text">Title</span>
-            <input
-                type="text"
-                id="title"
-                bind:value={$form.title}
-                class="input input-bordered w-full mt-2"
-            />
+            <input type="text" id="title" bind:value={$form.title} class="input input-bordered mt-2 w-full" />
             {#if $form.errors.title}
-                <div class="text-error text-sm mt-1">{$form.errors.title}</div>
+                <div class="text-error mt-1 text-sm">{$form.errors.title}</div>
             {/if}
         </div>
 
@@ -64,21 +59,15 @@
             <textarea
                 id="description"
                 bind:value={$form.description}
-                class="textarea textarea-bordered w-full mt-2"
+                class="textarea textarea-bordered mt-2 w-full"
                 rows="4"
             ></textarea>
             {#if $form.errors.description}
-                <div class="text-error text-sm mt-1">
-                    {$form.errors.description}
-                </div>
+                <div class="text-error mt-1 text-sm">{$form.errors.description}</div>
             {/if}
         </div>
 
-        <button
-            type="submit"
-            disabled={$form.processing}
-            class="btn btn-primary"
-        >
+        <button type="submit" disabled={$form.processing} class="btn btn-primary">
             {#if $form.processing}
                 <span class="loading loading-spinner"></span>
             {/if}
@@ -87,8 +76,8 @@
     </form>
 </div>
 
-<h2 class="text-2xl font-bold mb-2">Items</h2>
-<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+<h2 class="mb-2 text-2xl font-bold">Items</h2>
+<div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
     {#if items.length > 0}
         {#each items as item}
             <div class="card border border-slate-300 transition duration-200">
@@ -102,10 +91,7 @@
                     {#if item.description}
                         <p>{item.description}</p>
                     {/if}
-                    <button
-                        class="btn btn-remove"
-                        onclick={() => deleteItem(item.id)}
-                    >
+                    <button class="btn btn-remove" onclick={() => deleteItem(item.id)}>
                         <span class="i-lucide-trash"></span>
                         Delete Item
                     </button>
@@ -113,8 +99,8 @@
             </div>
         {/each}
     {:else}
-        <div class="col-span-full alert alert-info">
-            <span class="i-lucide-info shrink-0 w-6 h-6"></span>
+        <div class="alert alert-info col-span-full">
+            <span class="i-lucide-info h-6 w-6 shrink-0"></span>
             <span>No items available.</span>
         </div>
     {/if}
